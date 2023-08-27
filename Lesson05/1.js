ff70d6ec9e84e515d4d024cae42714936c7d20fa
Alena Zvereva <angryvex@gmail.com>
2023-08-27 23:13:46 +0300

    Add Lesson05

---
+++ Lesson05/1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+Дан объект numbers. 
+Необходимо в консоль вывести все значения объекта, которые больше или равны 3.
+*/
+
+const numbers = {
+  key1: 12,
+  key2: 2,
+  key3: 4,
+  key4: 1,
+  key5: -244,
+  key6: 0,
+  key7: 7,
+};
+
+console.log(Object.values(numbers).filter((value) => value > 3));
